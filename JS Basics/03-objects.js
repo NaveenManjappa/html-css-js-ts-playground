19b68167bf26c9bb1c306b6b16ae71a93fb7ba17
Naveen Manjappa <NManjappa@manh.com>
2025-01-13 07:30:09 +0000

    js-basics Primitives and objects

--- JS Basics/03-objects.js
+++ JS Basics/03-objects.js
@@ -31,7 +31,7 @@ circle2 = createCircle(2);
 console.log(circle2);
 
 //Constructor function
-console.log('-----Constructor Functios----');
+console.log('-----Constructor Functions----');
 function Circle(radius){
   this.radius = radius;
   this.draw = function(){
@@ -63,6 +63,55 @@ console.log(circle3.constructor);
 new String(); // '',"",``
 new Boolean();// true,false
 new Number();// 1,2,3 ...
+new Function();//new function
 
 let x='';
-console.log(x.constructor)
+console.log(x.constructor);
+
+//Functions are objects
+console.log('-----Functions are objects----');
+function CircleObject(radius){
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  }
+}
+
+console.log('Func name:',CircleObject.name);
+console.log('Func length:',CircleObject.length);
+
+const otherCircle = {};
+CircleObject.call(otherCircle,1);
+CircleObject.apply({},[1,2,3]);
+console.log(otherCircle);
+
+const Circle10 = new Function('radius',`
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  }
+`);
+
+const another = new Circle10(1);
+console.log(another);
+
+//Value types(Primitives) - Number,String,Boolean,Symbol,undefined,null -- copied by their value
+//Reference types(Objects) - Objects,functions and arrays -- copied by their reference
+console.log('-----Value vs Reference types----');
+let xx = 10;
+let y = xx;
+
+xx = 20
+console.log('xx:',xx);
+
+let number = 10;
+let marks = { total:0 };
+
+function increase(number,marks){ //only value gets copied
+  number++;
+  marks.total++;
+}
+increase(number,marks);
+console.log('Number:',number);
+console.log('Marks:',marks.total);
+
