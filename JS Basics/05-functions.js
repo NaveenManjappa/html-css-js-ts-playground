92846f651c4ed1b755844374ac071533dc2c8820
Naveen Manjappa <NManjappa@manh.com>
2025-01-21 07:22:52 +0000

    js-basics Function hoisting,arguments and rest operator

--- JS Basics/05-functions.js
+++ JS Basics/05-functions.js
@@ -1,3 +1,5 @@
+console.log('---------Function declaration and expression---------');
+walk();//works because of hoisting
 //Function declaration
 function walk() {
   console.log('Walk');
@@ -11,3 +13,19 @@ const run = function() {
 const move = run;
 run();
 move();
+
+console.log('---------Arguments---------');
+function sum() {
+  console.log(arguments);
+  const total = Array.from(arguments).reduce((acc,cv) => acc + cv,0 );
+  return total;
+}
+
+console.log(sum(1,2,3,4,5,10,15,20,40));
+
+console.log('---------Rest operator---------');
+function add(discount,...args) {//Rest parameter must be last formal parameter
+  return args.reduce((a,b) => a + b);
+}
+
+console.log(add(1,2,3,4,5));
