afa29093deb51dcce5a735a61fa1500cf81c53c4
Naveen Manjappa <NManjappa@manh.com>
2025-02-12 20:02:08 +0000

    js-oop Method overriding

--- JS Oop/Prototypical Inheritance/01-prototypical-inheritance.js
+++ JS Oop/Prototypical Inheritance/01-prototypical-inheritance.js
@@ -1,19 +1,36 @@
-function Shape() {
-
+function Shape(color) {
+  this.color = color;
 }
 
 Shape.prototype.duplicate = function() {
   console.log('duplicate');
 }
 
-function Circle(radius) {
-  this.radius = radius;
+function extend(Child,Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
 }
 
-Circle.prototype = Object.create(Shape.prototype);
+function Circle(radius,color) {
+  Shape.call(this,color);
+  this.radius = radius;
+}
+extend(Circle,Shape);
+
+Circle.prototype.duplicate = function() {
+  Shape.prototype.duplicate.call(this);
+  console.log('circle duplicate');
+}
+
 Circle.prototype.draw = function() {
   console.log('draw');
 }
 
+function Square(size){
+  this.size = size;
+}
+
+extend(Square,Shape);
+
 const s = new Shape();
-const c = new Circle(1);
+const c = new Circle(1,'red');
