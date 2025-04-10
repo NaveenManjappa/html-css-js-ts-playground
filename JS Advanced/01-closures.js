934ad70cbef4c13e7cf75455449b004a17f335aa
Naveen Manjappa <NManjappa@manh.com>
2025-04-10 13:29:31 +0100

    JS-advanced Closures recap

---
+++ JS Advanced/01-closures.js
@@ -0,0 +1,39 @@
+function createCounter(){
+  let count = 0;
+  return {
+    increment: function() {
+      count++;
+    },
+    getCount:function(){
+      return count;
+    }
+  }
+}
+
+let myCounter = createCounter();
+
+myCounter.increment();
+myCounter.increment();
+
+let myCounter2 = createCounter();
+myCounter2.increment();
+
+console.log(myCounter.getCount());
+console.log(myCounter2.getCount());
+
+//Function factories
+function createLogger(prefix){
+  let logPrefix = '[' + prefix +'] ';
+
+  function logMessage(message){
+    console.log(logPrefix + message)
+  }
+
+  return logMessage;
+}
+
+let authLogger = createLogger('Auth Service');
+let orderLogger = createLogger('Order Service');
+
+authLogger('Login successful');
+orderLogger('Order placed!');
