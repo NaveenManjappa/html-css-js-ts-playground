993d9429c384ab23cecc81a6190757473e36aa46
Naveen Manjappa <NManjappa@manh.com>
2025-04-14 15:23:27 +0100

    JS-advanced RxJS operators

--- JS Advanced/02-rxjs.js
+++ JS Advanced/02-rxjs.js
@@ -3,9 +3,11 @@ const { fromEvent } = rxjs;
 
 fromEvent(document, 'click').subscribe(() => console.log('Clicked!'));
 
-const { of } = rxjs;
+const { of,interval } = rxjs;
+const { filter,map,tap,take } = rxjs;
 
-const numberObservable$ = of(1,2,3,4,5);
+
+
 
 const observer = {
   next:(value) => console.log(`Received value: ${value}`),
@@ -13,6 +15,45 @@ const observer = {
   complete:()=>console.log('Obervable finished')
 };
 
+const numberObservable$ = of(1,2,3,4,5);
 console.log('Subscribing..');
 const subscription = numberObservable$.subscribe(observer);
-console.log('Subscribed')
+console.log('Subscribed');
+
+
+//Operators
+const numberObs$=of(1,2,3,4,5,6);
+
+const oddObs$= numberObs$.pipe(
+filter(value => value % 2 !== 0)
+);
+
+console.log('Odd numbers');
+oddObs$.subscribe(val => console.log(val));
+
+const evenDoubled$ = numberObs$.pipe(
+  filter(v => v % 2 === 0),
+  map(v => v * 2)
+);
+
+console.log('Even doubled');
+evenDoubled$.subscribe(v => console.log(v));
+
+//tap operator
+console.log('Tap operator');
+const tenNums$= numberObs$.pipe(
+  tap(v => console.log(`Tap before filter:${v}`)),
+  filter(v => v % 2 === 0),
+  tap(v => console.log(`Tap after filter:${v}`)),
+  map(v => v * 10)
+);
+tenNums$.subscribe(v => console.log(v));
+
+//take operator
+console.log('Take operator');
+const numEverySec$ = interval(1000);
+const takeNum$ = numEverySec$.pipe(
+  take(30)
+);
+
+takeNum$.subscribe(observer);
