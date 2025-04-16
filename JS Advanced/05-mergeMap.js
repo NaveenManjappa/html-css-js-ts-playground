61877553b91854172adf3c08dbb3741fbe0e7f66
Naveen Manjappa <NManjappa@manh.com>
2025-04-16 17:35:02 +0100

    JS-advanced RxJs Transformation operators

---
+++ JS Advanced/05-mergeMap.js
@@ -0,0 +1,25 @@
+const { fromEvent,interval } = rxjs;
+const { pipe,mergeMap,map,take } = rxjs.operators;
+
+const click$ = fromEvent(document.getElementById('myButton'),'click');
+
+const result$ = click$.pipe(
+  mergeMap((event) => {
+    console.log('button clicked');
+    return interval(500).pipe(
+      map(i => `Result from click ${event.timeStamp.toFixed(0)}: value ${i}`),
+      take(3)
+    );
+  })
+);
+
+console.log('Subscribing to the results');
+const subscription  = result$.subscribe({
+next: value => console.log('next',value),
+complete: () => console.log('Observable completed')
+});
+
+setTimeout(()=>{
+  console.log('Unsubscribing');
+  subscription.unsubscribe();
+},10000)
